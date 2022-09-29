8c96e863123e117d842421958fa2a84ff97832c5
hotekk <114708512+hotekk@users.noreply.github.com>
2022-09-30 03:55:15 +0400

    Add files via upload

---
+++ hotek.js
@@ -0,0 +1,10 @@
+function homa() {
+    document.getElementById("homepage").style.display = ("block")
+    document.getElementById("new").style.display = ("none")
+}
+
+
+function news() {
+    document.getElementById("new").style.display = ("block")
+    document.getElementById("homepage").style.display = ("none")
+}
